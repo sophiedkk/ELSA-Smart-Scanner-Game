ed2520738e5d2709512f48a2c15f75a6c1a071c6
rickdkk <r.de.klerk@pl.hanze.nl>
2024-03-30 16:04:35 +0100

    Change resource loading

--- game/controllers/object_controller.gd
+++ game/controllers/object_controller.gd
@@ -1,28 +1,31 @@
-extends Node2D
 class_name ObjectController
+extends Node2D
 
-#Potentially this can be rearranged into an array of resources instead?
-#So you load the default empty object and then put the resource into it
-@export var level_objects: Array[PackedScene]
+@export var level_object_scene: PackedScene
+@export var level_objects: Array[AnalysisObjectData]
+
 var current_object_index = 0
 var current_object: BaseObject
 var current_object_acceptable: bool
-
 #Reference to the colors in the enumerator of the object
 var color_palette: Array[AnalysisObjectData.ObjectTypeColors]
-#Reference to the object scene that works as a button
-@onready var color_palette_object: = preload("res://game/gui/color_rectangle.tscn")
 #Reference to the object in the scene
 var color_palette_square: PaletteSquare
 #An array that stores squares currently presented on screen
 var color_palette_squares: Array[PaletteSquare]
 
+#Reference to the object scene that works as a button
+@onready var color_palette_object: = preload("res://game/gui/color_rectangle.tscn")
+
+
 func show_new_object():
-	if (current_object != null):
+	if current_object != null:
 		current_object.queue_free()
-	if (current_object_index < len(level_objects)):
-		var object_path = load(level_objects[current_object_index].resource_path)
-		current_object = object_path.instantiate()
+
+	var new_object_properties = level_objects.pop_front()
+	if new_object_properties:
+		current_object = level_object_scene.instantiate()
+		current_object.object_properties = new_object_properties
 		add_child(current_object)
 		clear_previous_palette()
 		show_new_object_palette()
@@ -32,8 +35,9 @@ func show_new_object():
 		clear_previous_palette()
 		print(color_palette, color_palette_squares)
 
+
 func show_new_object_palette():
-	if (current_object != null):
+	if current_object != null:
 		color_palette = current_object.object_properties.object_type_colors
 		print(color_palette)
 		for i in range(len(color_palette)):
@@ -48,10 +52,11 @@ func show_new_object_palette():
 			color_palette_square.position = Vector2(i * 40, -70)
 			color_palette_squares.push_back(color_palette_square)
 
+
 func clear_previous_palette():
-	if (color_palette_squares != null):
-		for i in len(color_palette_squares):
-			color_palette_squares[i].queue_free()
+	if color_palette_squares != null:
+		for square in color_palette_squares:
+			square.queue_free()
 		color_palette.clear()
 		#Not sure if I have to clear the array here
 		color_palette_squares.clear()
