5b2091ef0868d826323b396e8da96a2f18d0a671
Kostya Deputatov <kostyadeputatov@gmail.com>
2024-04-15 11:09:01 +0200

    Updated cards
    1. Imported temporary (wacky) cards for some of the assets
    2. Some errors fix

--- game/objects/object_card.gd
+++ game/objects/object_card.gd
@@ -60,4 +60,4 @@ func _on_area_2d_body_entered(body: ObjectRectangle):
 func _on_area_2d_body_exited(body):
 	if body.is_in_group("dropable"):
 		is_inside_droppable = false
-	body_ref.remove_object_card()
+		body.remove_object_card()
