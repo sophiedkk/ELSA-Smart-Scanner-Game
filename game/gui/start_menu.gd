25c2505e7784092dfeca31566d1c94d30dd32f3b
rickdkk <r.de.klerk@pl.hanze.nl>
2024-04-22 16:38:26 +0200

    Add locales

--- game/gui/start_menu.gd
+++ game/gui/start_menu.gd
@@ -7,14 +7,15 @@ extends CenterContainer
 
 
 func _ready():
+	TranslationServer.set_locale("nl")
 	if (LevelTransition.blacked_out == true):
 		await LevelTransition.fade_from_black()
 	start_game_button.grab_focus()
-	
-func _on_start_game_button_pressed(): 
+
+func _on_start_game_button_pressed():
 	await LevelTransition.fade_to_black()
 	get_tree().change_scene_to_file("res://game/levels/level_one.tscn")
- 
+
 
 func _on_quit_game_button_pressed():
 		get_tree().quit()
