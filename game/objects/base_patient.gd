dc4baf73607f35185b36589a9df63d6abcd018c1
sophiedkk <r.de.klerk@pl.hanze.nl>
2024-10-01 12:01:48 +0200

    Prepare Windows build

--- game/objects/base_patient.gd
+++ game/objects/base_patient.gd
@@ -6,12 +6,7 @@ extends Sprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process_input(true)
-	
+
 	if not patient_information:
 		return
 	texture = patient_information.patient_sprite
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
